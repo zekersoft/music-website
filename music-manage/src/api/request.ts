8a704724f05a89d5354c9963253d4d091ecb6f51
zeker <55832583+zekersoft@users.noreply.github.com>
2024-01-04 20:31:27 +0800

    适配请求头content-type

--- music-manage/src/api/request.ts
+++ music-manage/src/api/request.ts
@@ -7,7 +7,7 @@ axios.defaults.timeout = 5000 // 超时时间设置
 axios.defaults.withCredentials = true // true允许跨域
 axios.defaults.baseURL = BASE_URL
 // Content-Type 响应头
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8'
 
 // 响应拦截器
 axios.interceptors.response.use(
